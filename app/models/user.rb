fe661bd1f8ac2433baa35754db4c4160e7f0f4fb
tom2828 <yhewestwing4@yahoo.co.jp>
2022-03-25 21:50:56 +0900

    修正②

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
 
          validates :nickname,               presence: true
          validates :family_name,
-         format: { with: /\A[ぁ-んァ-ン一-龥]/},
+         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/},
          presence: true
          validates :first_name,
-         format: { with: /\A[ぁ-んァ-ン一-龥]/},
+         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/},
          presence: true
          validates :family_name_kana,
          format: { with: /\A[ァ-ヶー-]+\z/},

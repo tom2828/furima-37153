cc014b8f7d6f942d775ff89c10add1255a4bb807
tom2828 <yhewestwing4@yahoo.co.jp>
2022-03-25 18:39:56 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,               presence: true
-         validates :encrypted_password,     presence: true
-         validates :family_name,            presence: true
-         validates :family_name_kana,       presence: true
-         validates :first_name,             presence: true
-         validates :first_name_kana,        presence: true
+         validates :family_name,
+         format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+         presence: true
+         validates :first_name,
+         format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+         presence: true
+         validates :family_name_kana,
+         format: { with: /\A([ァ-ン]|ー)+\z/},
+         presence: true
+         validates :first_name_kana,
+         format: { with: /\A([ァ-ン]|ー)+\z/},
+         presence: true
          validates :birth_day,              presence: true
 
          has_many :items
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2022_03_21_100038) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nicname", null: false
+    t.string "nickname", null: false
     t.string "family_name", null: false
     t.string "first_name", null: false
     t.string "family_name_kana", null: false

d8a073795cd3ee5abad2d98ec66590dc15e626c0
tom2828 <yhewestwing4@yahoo.co.jp>
2022-03-27 23:25:04 +0900

    商品情報編集画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id]) 
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+      if @item.valid?
+        redirect_to item_path(item_params)
+      else
+        # NGであれば、エラー内容とデータを保持したままeditファイルを読み込み、エラーメッセージを表示させる
+        render 'edit'
+      end
+  end
+
+
+
+
+
+
   private
 
   def item_params

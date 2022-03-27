fc9a5b04c43aa141ccc17131901a1a0ae67979a7
tom2828 <yhewestwing4@yahoo.co.jp>
2022-03-27 11:02:29 +0900

    修正①

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,9 +18,8 @@ class Item < ApplicationRecord
   has_one_attached :image
 
  #バリデーション設定
-    validates :user_id,                    presence: true
     validates :image,                      presence: true
-    validates :item_name,                       presence: true
+    validates :item_name,                  presence: true
     validates :description,                presence: true
     validates :category_id,                presence: true,numericality: { other_than: 0 }
     validates :status_id,                  presence: true,numericality: { other_than: 0 }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,9 +13,9 @@ RSpec.describe Item, type: :model do
  
     context '出品ができないとき' do
       it 'ユーザー登録している人でないと出品できない' do
-        @item.user_id = nil
+        @item.user_id = ""
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist', "User can't be blank")
+        expect(@item.errors.full_messages).to include("User must exist")
       end
       it '画像がないと出品できない' do
         @item.image = nil
@@ -23,12 +23,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it '商品名が空欄だと出品できない' do
-        @item.item_name = nil
+        @item.item_name = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
       it '商品の説明が空欄だと出品できない' do
-        @item.description = nil
+        @item.description = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
@@ -58,7 +58,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Shipping day must be other than 0')
       end     
       it '価格が空欄だと出品できない' do
-        @item.price = nil
+        @item.price = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number')
       end
@@ -72,6 +72,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end     
+      it '価格は半角数字のみ保存可能であること' do
+        @item.price = "a"
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end   
     end
   end
 end

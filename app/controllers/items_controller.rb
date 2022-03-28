136d0676f625b2b1601cb9824d9113846e45d29f
tom2828 <yhewestwing4@yahoo.co.jp>
2022-03-28 11:49:21 +0900

    修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,8 +29,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-    if @item.valid?
+    if @item.update(item_params)
       redirect_to item_path(item_params)
     else
       # エラーメッセージを表示させる

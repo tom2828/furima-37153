f120d88a99d66040f2e5e1ec89afb51cbff52346
tom2828 <yhewestwing4@yahoo.co.jp>
2022-04-03 17:16:40 +0900

    修正②

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :order_form do
+    user_id { FactoryBot.create(:user).id }
+    item_id { FactoryBot.create(:item).id }
     post_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
     prefecture_id { Faker::Number.between(from: 1, to: 47) }
     city { Faker::Address.city }
